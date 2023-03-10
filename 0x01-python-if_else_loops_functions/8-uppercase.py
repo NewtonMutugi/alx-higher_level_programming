76daa769ca56f5804bfd05af72ba7876cd474c54
NewtonMutugi <newtonmut197@gmail.com>
2023-03-10 13:24:09 +0300

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in str:
-	if ord(i) >= 97 and ord(i) <= 122:
-	    i = chr(ord(i) - 32)
-	print("{}".format(i), end="")
+        if ord(i) >= 97 and ord(i) <= 122:
+            i = chr(ord(i) - 32)
+        print("{}".format(i), end="")
     print()
-
-
-if __name__ == "__main__":
-	    uppercase("holberton")
-    uppercase("Holberton School 98 Battery street")
-    uppercase("holberton")
-    uppercase("Holberton School 98 Battery street")
