a829411428e01fdc3277a89064fa16f8287ee6a4
NewtonMutugi <newtonmut197@gmail.com>
2023-03-10 18:38:04 +0300

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-import sys
-
 for i in range(122, 96, -1):
     letter = chr(i)
     condition = i % 2 != 0
-    print(letter.upper() if condition else letter, end='')
-sys.stdout.write('\n')
+    print("{}".format(letter.upper() if condition else letter), end='')
+print('\n', end='')
