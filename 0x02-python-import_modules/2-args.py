bc1e55a107107c66dd48f0d6374495617353b14b
NewtonMutugi <newtonmut197@gmail.com>
2023-03-11 15:19:29 +0300

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,9 @@ if __name__ == "__main__":
     args = sys.argv
     if len(args) == 0:
         print("0 arguments.")
+    elif len(args) == 2:
+        print("1 argument:")
+        print("1: {:s}".format(args[1]))
     else:
         print("{:d} arguments:".format(len(args) - 1))
         for i in range(1, len(args)):
