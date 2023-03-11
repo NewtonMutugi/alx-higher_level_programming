163aaf5abf117816d8f0ad61b1531fba7a23ab6d
NewtonMutugi <newtonmut197@gmail.com>
2023-03-11 15:02:10 +0300

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv
+    if len(args) == 0:
+        print("0 arguments")
+    else:
+        print("{} arguments".format(len(args) - 1))
+        for i in range(1, len(args)):
+            print("{}: {}".format(i, args[i]))
