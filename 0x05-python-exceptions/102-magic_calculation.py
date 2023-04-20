3ea3414968085722459dd81fe2a330a393bfee6f
NewtonMutugi <newtonmut197@gmail.com>
2023-04-21 02:13:14 +0300

    102-magic_calculation.py

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b):
-    result = 0  # Initialize result to 0
-
-    for i in range(1, 4):  # Loop from 1 to 3
+    result = 0
+    for i in range(1, 3):
         try:
             if i > a:
-                # Raise a ValueError if i is greater than a
-                raise ValueError('Too far')
-            # Perform the calculation and update the result
-            result += (a ** b) / i
-        except ValueError:
-            pass  # Ignore ValueError exceptions
-
-    result += a + b  # Add a and b to the result
-
-    return result  # Return the final result
+                raise Exception('Too far')
+            else:
+                result += a ** b / i
+        except:
+            result = b + a
+            break
+            return result
