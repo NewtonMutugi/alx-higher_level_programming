acd370021ad008dc9a3ab1e20cb436f335f5027d
NewtonMutugi <newtonmut197@gmail.com>
2023-04-21 11:02:18 +0300

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module defines a class Square"""
+
+
+class Square:
+    """This class defines a square
+    Attributes:
+        __size (int): size of the square"""
+
+    def __init__(self, size):
+        """Initializes the square
+        Args:
+        size (int): private size of the square"""
+        self.__size = size
