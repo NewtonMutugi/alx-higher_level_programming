1c02febba6277f4c020bb25993eedaa54c16e87a
NewtonMutugi <newtonmut197@gmail.com>
2023-05-20 21:30:06 +0300

    5-rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -82,3 +82,8 @@ class Rectangle(Base):
         for i in range(self.__height):
             print(" " * self.__x, end="")
             print("#" * self.__width)
+
+    def __str__(self):
+        """Return the string representation of the rectangle"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.__x, self.__y, self.__width, self.__height)
