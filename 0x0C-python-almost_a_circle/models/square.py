f7ed953b56b399a79e3996f94462aa0fea9fb53c
NewtonMutugi <newtonmut197@gmail.com>
2023-05-20 22:13:53 +0300

    10-main.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,19 +12,19 @@ class Square(Rectangle):
         self.size = size
         self.__x = x
         self.__y = y
-        self.__id = id
+        self.__id = None
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
         """Getter for size"""
-        return self.__width
+        return self.width
 
     @size.setter
     def size(self, value):
         """Setter for size"""
-        self.__width = value
-        self.__height = value
+        self.width = value
+        self.height = value
 
     def __str__(self):
         """Returns string representation of Square"""
