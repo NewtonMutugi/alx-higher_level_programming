c4b7d44b861f2a1cd49261bf418ef514d78dac8b
NewtonMutugi <newtonmut197@gmail.com>
2023-06-12 23:34:31 +0300

    Added 11-second_biggest.js

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+if (isNaN(process.argv[2])) {
+  console.log(0);
+} else {
+  const arr = [];
+  for (let i = 2; i < process.argv.length; i++) {
+    arr.push(parseInt(process.argv[i]));
+  }
+  arr.sort(function (a, b) { return a - b; });
+  console.log(arr[arr.length - 2]);
+}
