05fbd39c3d817d202e0bd06d6deba3c1eb5fdb9b
NewtonMutugi <newtonmut197@gmail.com>
2023-06-23 21:26:45 +0300

    model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,19 +3,22 @@
 
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+# from sqlalchemy.orm import relationship
 
 
 Base = declarative_base()
 
 
 class State(Base):
-    """This class defines a state by various attributes"""
+    """
+    This class defines a state by various attributes
+    Attributes:
+      __tablename__(str): The name of the table in the database
+      id(int) will be the id of the table and it will be the primary key
+      name(str) will be the name of the state and it can't be null
+    """
+
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all, delete", backref="state")
-
-    def __init__(self, name):
-        """This method initializes a state with a name"""
-        self.name = name
+    # cities = relationship("City", cascade="all, delete", backref="state")
