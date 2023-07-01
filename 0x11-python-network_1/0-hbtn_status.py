272c6c2a1f1d6bddc5f91ad2927a1f650fe1f77d
NewtonMutugi <newtonmut197@gmail.com>
2023-07-01 09:33:46 +0300

    fixed 2-post_email

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """Script to fetch https://intranet.hbtn.io/status"""
 import urllib.request
-import sys
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen(sys.argv[1]) as response:
-        html = response.read()
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
+        page = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode("utf-8")))
+        print("\t- type: {}".format(type(page)))
+        print("\t- content: {}".format(page))
+        print("\t- utf8 content: {}".format(page.decode("utf-8")))
