9ec9e84f31d25020a61e5066e48a1eb50a834965
NewtonMutugi <newtonmut197@gmail.com>
2023-07-01 12:00:21 +0300

    Modified 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,7 +9,8 @@ import sys
 if __name__ == "__main__":
     url = 'https://api.github.com/repos/{}/{}/commits'.format(
         sys.argv[2], sys.argv[1])
-    r = requests.get(url)
+    header = {"Accept": "application/vnd.github.v3+json"}
+    r = requests.get(url, headers=header)
     try:
         json = r.json()
         for i in range(10):
