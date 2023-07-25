84e50ba8355e6793b80fb7df1b4785130452618c
NewtonMutugi <newtonmut197@gmail.com>
2023-07-25 21:35:58 +0300

    Added 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
+// script that reads and prints the content of a file.
 const fs = require('fs');
 const file = process.argv[2];
 fs.readFile(file, 'utf8', function (err, data) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(data.toString());
-    }
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data.toString());
+  }
 }
 );
-
