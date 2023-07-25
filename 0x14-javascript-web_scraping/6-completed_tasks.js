755c70bc2e03952e4e45209ef07ca5e8f78148d5
NewtonMutugi <newtonmut197@gmail.com>
2023-07-25 22:31:01 +0300

    Added 6-completed_tasks.js

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// script that gets the contents of a webpage and stores it in a file.
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const file = process.argv[3];
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(file, body, 'utf8', function (err) {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+}
+);
